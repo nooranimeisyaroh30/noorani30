cb74ad5a007c8206fe361b83cf098a6841351e79
nooranimeisyaroh30 <nooranimeisyaroh7@gmail.com>
2020-08-11 11:40:10 +0700

    first commit

---
+++ app/src/main/java/com/seedgrow/modultiga/MainActivity.java
@@ -0,0 +1,35 @@
+package com.seedgrow.modultiga;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    String TAG = "Main Activity" ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void input(View view) {
+        EditText inputNamaTimA = findViewById(R.id.edit_Tim_A);
+        EditText inputNamaTimB = findViewById(R.id.edit_Tim_B);
+
+        String namaTimA = inputNamaTimA.getText().toString();
+        Log.d(TAG, namaTimA);
+
+        String namaTimB = inputNamaTimB.getText().toString();
+        Log.d(TAG, namaTimB);
+
+        Intent intent = new Intent(this, CounterActivity.class);
+        intent.putExtra("Tim A", namaTimA);
+        intent.putExtra("Tim B", namaTimB);
+        startActivity(intent);
+    }
+}
